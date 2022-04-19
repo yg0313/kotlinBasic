c8d3cb188b288cf84e8fedc683014e8bfe48a1c8
Kim yonggwan <sssw0101@naver.com>
2022-04-19 18:34:14 +0900

    함수

---
+++ src/kotlin/Test.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) { // 메인함수
+    var num = 10;
+    println(num)
+    println(helloWorld())
+    println(add(1,2))
+}
+
+fun helloWorld() : Unit{ // return 타입이 없으면 Unit으로 설정 (생략가능)
+    println("helloWorld")
+}
+
+fun add(a:Int, b:Int) : Int{
+    println("덧셈")
+    return a+b
+}
