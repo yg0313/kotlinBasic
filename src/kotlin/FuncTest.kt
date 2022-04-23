d62f35380b35ed0de3e6b036557366b1e203b2aa
Kim yonggwan <sssw0101@naver.com>
2022-04-23 19:02:39 +0900

    함수 fun

---
+++ src/kotlin/FuncTest.kt
@@ -0,0 +1,17 @@
+fun main(args : Array<String>){
+    voidAdd(1,2)
+    println(returnAdd(1,2))
+    println(defaultNum(1))
+}
+
+fun voidAdd(a:Int, b:Int){ // 반환값이 없음
+    println(a+b)
+}
+
+fun returnAdd(a:Int, b:Int) : Int{ // 반환값이 있음
+    return a+b
+}
+
+fun defaultNum(a:Int, b:Int=2) : Int{   // 인자에 디폴트값 부여
+    return a+b
+}
