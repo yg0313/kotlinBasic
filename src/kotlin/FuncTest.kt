efa58590810198415ceec70be4bbedfcd279a0a6
Kim yonggwan <sssw0101@naver.com>
2022-04-24 20:54:29 +0900

    함수 fun
    가변인자 활용

--- src/kotlin/FuncTest.kt
+++ src/kotlin/FuncTest.kt
@@ -1,7 +1,9 @@
 fun main(args : Array<String>){
-    voidAdd(1,2)
-    println(returnAdd(1,2))
-    println(defaultNum(1))
+//    voidAdd(1,2)
+//    println(returnAdd(1,2))
+//    println(defaultNum(1))
+
+    varNum(1,2,3,4)
 }
 
 fun voidAdd(a:Int, b:Int){ // 반환값이 없음
@@ -14,4 +16,10 @@ fun returnAdd(a:Int, b:Int) : Int{ // 반환값이 있음
 
 fun defaultNum(a:Int, b:Int=2) : Int{   // 인자에 디폴트값 부여
     return a+b
+}
+
+fun varNum(vararg num:Int){ // 가변인자 활용
+    for (number in num){
+        print(number)
+    }
 }
