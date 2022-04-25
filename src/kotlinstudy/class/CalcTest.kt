d70cc9265d3ffd27cbe6ebdba94922d4623f7f11
Kim yonggwan <sssw0101@naver.com>
2022-04-25 22:07:32 +0900

    클래스 - 생성자가 없는 클래스

---
+++ src/kotlinstudy/class/CalcTest.kt
@@ -0,0 +1,6 @@
+package kotlinstudy.`class`
+
+fun main(args : Array<String>){
+    val calculator = Calculator();
+    calculator.sum(1,2)
+}
---
+++ src/kotlinstudy/class/Calculator.kt
@@ -0,0 +1,8 @@
+package kotlinstudy.`class`
+
+// 생성자가 없는 클래스
+class Calculator {
+    fun sum(a:Int, b:Int){
+        println(a+b)
+    }
+}
