ca76574cf1a9fe8f78e1a6fd03ef54aeeac50e9d
Kim yonggwan <sssw0101@naver.com>
2022-04-25 22:07:41 +0900

    패키지명 변경

--- src/kotlinstudy/Test.kt
+++ src/kotlinstudy/Test.kt
@@ -3,6 +3,11 @@ fun main(args: Array<String>) { // 메인함수
     println(num)
     println(helloWorld())
     println(add(1,2))
+    var emptyArr = emptyArray<Int>()
+    println(emptyArr.toString())
+
+    var nullArr = arrayOfNulls<Int>(5)
+    println(nullArr.size)
 }
 
 fun helloWorld() : Unit{ // return 타입이 없으면 Unit으로 설정 (생략가능)
